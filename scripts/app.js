a66caf152091d82d7b3f8edb1a01daa207166282
PrameethaUmmadi <prameethaummadi@gmail.com>
2015-09-15 20:48:50 -0400

    search and tags
    fix issues with search and tags

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,19 @@
-var myApp = angular.module('myApp', ['ngResource','autocomplete']);
+//main module
+var myApp = angular.module('myApp', ['ngResource','autocomplete','angularUtils.directives.dirPagination']);
   
+//controller  
 myApp.controller('PhotoController',[ '$scope','$http', function PhotoController($scope, $http) {
-    $scope.movies = []
+    $scope.html = "photos.html";
+    $scope.gallery = []
     $scope.pics = [];
+    $scope.selectedIndex = "";
+    //url to fetch tags of a user
    var url = "https://api.flickr.com/services/rest/?method=flickr.tags.getListUser&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json&jsoncallback=getTags";
-    $http.jsonp("https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json").success(function (data) {
-        $scope.photos = data.photos;
-        $scope.pics = $scope.photos.photo;
-        angular.forEach($scope.photos.photo, function(item){
-            $scope.movies.push(item.title);
-         });
-    });
+   //url to fetch all the photos uploaded by a user
+   var getAllPicsUrl = "https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&format=json&jsoncallback=jsonFlickrApi";
    $http.jsonp(url);
+   $http.jsonp(getAllPicsUrl);
+   //callback functions
    getTags = function(data){
        $scope.tags = data.who.tags;
    }
@@ -19,13 +21,13 @@ myApp.controller('PhotoController',[ '$scope','$http', function PhotoController(
         $scope.photos = data.photos;
         $scope.pics = $scope.photos.photo;
         angular.forEach($scope.photos.photo, function(item){
-            $scope.movies.push(item.title);
+            $scope.gallery.push(item.title);
          });
     };
     
   $scope.doSomething = function(searchString){
      if(searchString.length == 0){
-        $scope.photos.photo = $scope.pics;
+       $scope.photos.photo = $scope.pics;
      }
   }
   $scope.doSomethingElse = function(searchString){
@@ -34,5 +36,16 @@ myApp.controller('PhotoController',[ '$scope','$http', function PhotoController(
            return item.title === searchString;
        });
   }
+  $scope.getPhotos = function(selectedTag,i){
+    $scope.selectedIndex = i;
+    selectedTag = selectedTag.toLowerCase();
+    var tag_based_search_url = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=a5e95177da353f58113fd60296e1d250&user_id=132365033@N08&tags="+selectedTag+"&format=json&jsoncallback=getPicsOfTags";
+    $http.jsonp(tag_based_search_url);
+    getPicsOfTags = function(data){
+        $scope.photos = data.photos;
+    }
+    $scope.html = "tagResult.html";
+   document.getElementsByName("searchBox")[0].value = '';
+}
 
 }]);
